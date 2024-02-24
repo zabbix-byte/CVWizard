9809a06d70162cb0a1f9a01c9dda484e83149dcf
zabbix-byte <zabbix@ztrunk.space>
2024-02-24 15:26:58 +0100

    [minor] Adding some stamps and memory handlers.

--- linkedincv/scraper/models.py
+++ linkedincv/scraper/models.py
@@ -5,12 +5,14 @@ from django.contrib.auth.models import User
 class UserProfileHtml(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     data = models.JSONField()
+    last_modified = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
 
 
 class UserCookie(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     cookie = models.TextField(unique=True, max_length=500)
     access_agree = models.BooleanField(default=True)
+    last_modified = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
 
     def __str__(self):
         return f'{self.user.username}::{self.cookie}'
